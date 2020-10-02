9556d7dab5c120d405857c1834451b677e07ef7e
mitsuru_yamaura <komayou326@gmail.com>
2020-10-02 20:56:48 +0900

    障害物に上下左右・回転の動作を実装。

--- Assets/Scripts/Obstacle_Needles.cs
+++ Assets/Scripts/Obstacle_Needles.cs
@@ -8,6 +8,12 @@ public class Obstacle_Needles : Hole
     [Header("移動時間。大きいほどゆっくり動く")]
     public float moveTime;
 
+    [Header("X軸の移動距離")]
+    public float posX;
+
+    [Header("Y軸の移動距離")]
+    public float posY;
+
     [Header("回転させる場合はスイッチいれる")]
     public bool isRotate;
 
@@ -15,8 +21,10 @@ public class Obstacle_Needles : Hole
     {
         base.Start();
 
-        transform.DOMoveX(2, moveTime).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+        // 移動。XとY両方に数値が入ると斜めに動く
+        transform.DOMove(new Vector2(posX, posY), moveTime).SetRelative().SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
 
+        // 回転
         if (isRotate) {
             transform.DORotate(new Vector3(0, 0, 360), 1.0f, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1, LoopType.Incremental);
         }
