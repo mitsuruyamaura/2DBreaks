f7fbd4840badab6a36a6d0f01fa05a415548badb
mitsuruyamaura <komayou326@gmail.com>
2022-10-21 11:40:37 +0900

    MosaicManager クラス内の処理を分割して役割分担化。まずは View のみ。

---
+++ Assets/Scripts/MosaicStage/Container/MainGameEntryPoint.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+public class MainGameEntryPoint : IStartable {
+
+
+
+    void IStartable.Start() {
+        
+    }
+}
---
+++ Assets/Scripts/MosaicStage/Container/MainGamePresenter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+public class MainGamePresenter {
+
+    // View クラス
+    [SerializeField]
+    private MainGameInfoView mainGameInfoView;
+
+    // Model クラス
+
+
+
+
+    public MainGamePresenter() {
+
+    }
+
+    // スタート
+
+    // 初期化
+
+    // ボタンなどの購読
+
+    // ステート監視
+
+    // リザルト判定
+
+}
